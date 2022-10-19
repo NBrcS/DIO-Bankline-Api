f2a89d8578812b06500fdb5034980829999083c8
Gabriel Nóbrega <carlosgabrielncs@gmail.com>
2022-10-19 13:14:50 -0300

    finished project

---
+++ src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -0,0 +1,52 @@
+package com.dio.santander.banklineapi.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Movimentacao {
+    private Integer id;
+    private LocalDateTime data;
+    private String descicao;
+    private Double valor;
+    private MovimentacaoTipo tipo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public String getDescicao() {
+        return descicao;
+    }
+
+    public void setDescicao(String descicao) {
+        this.descicao = descicao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public MovimentacaoTipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(MovimentacaoTipo tipo) {
+        this.tipo = tipo;
+    }
+}
